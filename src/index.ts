17817deafcfc03b7233d568d7dd0dfd1288b6ad5
0xcollins <chemerilcollins@gmail.com>
2025-11-12 19:20:04 +0100

    refactor(project): Restructure project and update TypeScript configuration

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (_req: Request, res: Response) => {
+    res.json({
+        message: "Hello World"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
